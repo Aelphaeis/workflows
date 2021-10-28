7802b114fcf21a860eb5790e2ea198bb3fef61bc
Joseph <aelphaeis@live.com>
2021-10-28 11:08:14 -0400

    init

---
+++ src/main/java/com/cruat/workflows/Program.java
@@ -0,0 +1,13 @@
+package com.cruat.workflows;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Program {
+
+	private static final Logger logger = LogManager.getLogger();
+
+	public static void main(String[] args) {
+		logger.info("Hello World");
+	}
+}
---
+++ src/test/java/com/cruat/workflows/ProgramTest.java
@@ -0,0 +1,12 @@
+package com.cruat.workflows;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ProgramTest {
+
+	@Test
+	public void testMain() {
+		assertTrue(true);
+	}
+}
